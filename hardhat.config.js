d280e9cf08f02c229cf894a6270610749722913e
JordiMartinezDev <jordiupc87@gmail.com>
2024-02-16 07:18:54 +0100

    Hardhat config + dotenv with network&wallet

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,16 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    hardhat: { chainId: 1337 },
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI_URL,
+      accounts: [process.env.WALLET_SECRET],
+    },
+    mainnet: {
+      url: process.env.POLYGON_MAINNET_URL,
+      accounts: [process.env.WALLET_SECRET],
+    },
+  },
   solidity: "0.8.24",
 };
