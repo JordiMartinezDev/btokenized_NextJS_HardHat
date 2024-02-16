7b1a6093f7fb9d2a6975c657e816a6bd07a59b1f
JordiMartinezDev <jordiupc87@gmail.com>
2024-02-16 07:39:24 +0100

    created Smart Contracts

---
+++ contracts/BasicNFT.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
+contract BasicNFT is ERC721URIStorage{
+
+    uint256 private _tokenId;
+
+}
