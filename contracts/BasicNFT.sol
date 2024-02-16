49ad78a5bd8714e4262e874600a98ffffb1643a8
JordiMartinezDev <jordiupc87@gmail.com>
2024-02-16 07:51:57 +0100

    Basic NFT contract created

--- contracts/BasicNFT.sol
+++ contracts/BasicNFT.sol
@@ -1,13 +1,32 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract BasicNft is ERC721 {
+    error BasicNft__TokenUriNotFound();
 
-contract BasicNFT is ERC721URIStorage{
+    mapping(uint256 tokenId => string tokenUri) private s_tokenIdToUri;
+    uint256 private s_tokenCounter;
 
-    uint256 private _tokenId;
+    constructor(string memory _name,string memory _symbol) ERC721(_name, _symbol) {
+        s_tokenCounter = 0;
+    }
 
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        if (ownerOf(tokenId) == address(0)) {
+            revert BasicNft__TokenUriNotFound();
+        }
+        return s_tokenIdToUri[tokenId];
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -6,8 +6,9 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 
-contract BasicNFT is ERC721URIStorage{
+contract MarketPlace is ERC721URIStorage{
 
     uint256 private _tokenId;
+    address contractAddress;
 
 }
